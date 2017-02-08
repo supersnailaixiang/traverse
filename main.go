b55aedb758aab80f84d8f0601b0e045bd9257cd7
chenaixiang <supersnailaixiang@gmail.com>
2017-02-08 11:48:00 +0800

    改变路径

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"traverse"
 )
 
 var rootDir *string = flag.String("d", "./tmp/", "traverse dir")
@@ -12,5 +11,5 @@ var outFile *string = flag.String("o", "", "ignore dir when traverse")
 func main() {
 	flag.Parse()
 	//	fmt.Println("-d 制定遍历文件  -i 制定忽略文件 -o 输出文件")
-	traverse.TraverseFile(*rootDir, *ignoreDir, *outFile)
+	TraverseFile(*rootDir, *ignoreDir, *outFile)
 }
--- traverse_test.go
+++ traverse_test.go
@@ -1,4 +1,4 @@
-package traverse
+package main
 
 import "testing"
 
