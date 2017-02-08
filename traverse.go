440e2678f558f683c97318b0ef328f19542471f4
chenaixiang <supersnailaixiang@gmail.com>
2017-02-08 08:58:45 +0800

    分离路径

--- traverse.go
+++ traverse.go
@@ -3,7 +3,6 @@ package traverse
 import (
 	"bufio"
 	"crypto/sha1"
-	"flag"
 
 	"fmt"
 	"io/ioutil"
@@ -53,14 +52,4 @@ func TraverseFile(rootDir, ignoreDir, outFile string) {
 	if err != nil {
 		fmt.Printf("filepath.Walk() returned %v\n", err)
 	}
-}
-
-var rootDir *string = flag.String("d", "./tmp/", "traverse dir")
-var ignoreDir *string = flag.String("i", "", "ignore dir when traverse")
-var outFile *string = flag.String("o", "", "ignore dir when traverse")
-
-func main() {
-	flag.Parse()
-	//	fmt.Println("-d 制定遍历文件  -i 制定忽略文件 -o 输出文件")
-	TraverseFile(*rootDir, *ignoreDir, *outFile)
 }
